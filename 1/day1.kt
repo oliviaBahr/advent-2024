93ee19d2c0d263368efc799c8cc27382a58bd28c
Olivia Bahr <ob.bahr@gmail.com>
2024-12-01 14:57:07 -0800

    Improve kotlin impl

--- 1/day1.kt
+++ 1/day1.kt
@@ -1,22 +1,27 @@
 import java.io.File
+import kotlin.math.abs
 
 fun getLists(): Pair<List<Int>, List<Int>> {
     val lines = File("./input.txt").readLines()
     
-    val pairs = lines.map { line ->
-        line.split("\\s+".toRegex())
-            .map { it.trim().toInt() }
-            .let { it[0] to it[1] }
-    }
+    val (l1, l2) = lines.map { line ->
+        val (left, right) = line.split(Regex("\\s+"))
+        left.toInt() to right.toInt()
+    }.unzip()
 
-    val (l1, l2) = pairs.unzip()
     return l1.sorted() to l2.sorted()
+}
+
+fun part1(l1: List<Int>, l2: List<Int>): Int {
+    return l1.zip(l2).sumOf { (a, b) -> abs(a - b) }
+}
+
+fun part2(l1: List<Int>, l2: List<Int>): Int {
+    return l1.sumOf { lItem -> lItem * l2.count { it == lItem } }
 }
 
 fun main() {
     val (l1, l2) = getLists()
-    val a1 = l1.zip(l2).sumOf { (a, b) -> kotlin.math.abs(a - b) }
-    val a2 = l1.sumOf { lItem -> lItem * l2.count { it == lItem } }
-    println(a1)
-    println(a2)
+    println(part1(l1, l2))
+    println(part2(l1, l2))
 }
